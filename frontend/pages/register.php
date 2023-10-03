7f0ea87394744248983e1e90552e5ec4ebaa460b
MakotoAi <136439887+MakotoAi@users.noreply.github.com>
2023-10-03 19:57:52 +0700

    Update register.php

--- frontend/pages/register.php
+++ frontend/pages/register.php
@@ -1,8 +1,27 @@
 <?php
     include_once("../header.php");
 ?>
+<link rel="stylesheet" href="../../assets/style.css">
 
-<!-- Body Register -->
+<div class="first">
+    <img src="../../assets/img/humanikastore-circle.png" alt="">
+    <h1>Humanika <br> Store</h1>
+</div>
+
+<form action="">
+    <h1>Register Account</h1>
+    <label for="">Username</label>
+        <input type="text" placeholder="Masukan Username">
+    <label for="">Email</label>
+        <input type="text" placeholder="Masukan Email">
+    <label for="">Password</label>
+            <input type="text" placeholder="Masukan Password">
+    <label for="">Re Type: Password</label>
+            <input type="text" placeholder="Masukan Ulang Password">
+            <a href="login.php">Sudah punya akun? Login disini!</a>
+    <center><button>Daftar</button></center>
+</form>
+
 
 <?php
     include_once("../footer.php");
