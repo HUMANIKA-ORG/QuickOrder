05424e6764dbc8771e88ff90243c11f3ea32b01e
MakotoAi <hanifdrham007@gmail.com>
2023-10-03 19:51:33 +0700

    regist

--- frontend/pages/register.php
+++ frontend/pages/register.php
@@ -1,8 +1,26 @@
 <?php
     include_once("../header.php");
 ?>
+<link rel="stylesheet" href="../../assets/style.css">
 
-<!-- Body Register -->
+<div class="first">
+    <img src="../../assets/img/humanikastore-circle.png" alt="">
+    <h1>Humanika <br> Store</h1>
+</div>
+
+<form action="">
+    <label for="">Username</label>
+        <input type="text" placeholder="Masukan Username">
+    <label for="">Email</label>
+        <input type="text" placeholder="Masukan Email">
+    <label for="">Password</label>
+            <input type="text" placeholder="Masukan Password">
+    <label for="">Re Type: Password</label>
+            <input type="text" placeholder="Masukan Ulang Password">
+            <a href="">Sudah punya akun? Login disini!</a>
+    <center><button>Daftar</button></center>
+</form>
+
 
 <?php
     include_once("../footer.php");
