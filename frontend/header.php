5871cd832367a5d0b2b0ac6ad3d33696fc637b8c
AziSudarya1 <azisudarya98@gmail.com>
2023-10-03 18:54:58 +0700

    add css dll

--- frontend/header.php
+++ frontend/header.php
@@ -5,7 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Quick Order | HUMANIKA</title>
     <link rel="stylesheet" href="../vendor/bootstrap5-3.min.css">
-    <link rel="stylesheet" href="../assets/style.css">
   </head>
   <body>
     
--- frontend/pages/login.php
+++ frontend/pages/login.php
@@ -2,12 +2,23 @@
     include_once("../header.php");
 
 ?>
+<link rel="stylesheet" href="../../assets/style.css">
 
-
-<div class="fist">
+<div class="first">
     <img src="" alt="">
     <h1>Humanika <br> Store</h1>
 </div>
+
+<form action="" >
+    <h1>Login Account</h1>
+        <b><label for="">Username</label></b>
+        <input type="text" placeholder="Masukan Username">
+        
+        <b><label for="">Password</label></b>
+        <input type="text" placeholder="Masukan Password">
+    <b><p>Belum punya akun <a href="">Register</a> Disini</p></b>
+        <center><button>Login</button></center>
+</form>
 
 <?php
     include_once("../footer.php");
